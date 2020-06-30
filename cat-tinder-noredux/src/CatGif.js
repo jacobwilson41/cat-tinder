df2e41f9128ec4fd34d8f450c95d68e5f3b4229b
Jacob Wilson <jacobcharles96@gmail.com>
2020-06-30 12:24:16 -0400

    styling

--- cat-tinder-noredux/src/CatGif.js
+++ cat-tinder-noredux/src/CatGif.js
@@ -7,25 +7,30 @@ const CatGif = ({ url, getGif }) => {
   };
 
   return (
-    <div className="flex-container">
-      <img onClick={handleClick} src={url} alt="cat"></img>
-      <div className="kitty">DM me</div>
-    </div>
-
-    // <div className="demo">
-    //   <header className="demo__header"></header>
-    //   <div className="demo__content">
-    //     <div className="demo__card-cont">
-    //       <div className="demo__card">
-    //         <img onClick={handleClick} src={url} alt="cat"></img>;
-    //         <div className="demo__card__choice m--reject"></div>
-    //         <div className="demo__card__choice m--like"></div>
-    //         <div className="demo__card__drag"></div>
-    //       </div>
-    //     </div>
-    //     <p className="demo__tip">Swipe left or right</p>
-    //   </div>
+    // <div className="flex-container">
+    //   <img onClick={handleClick} src={url} alt="cat"></img>
+    //   <div className="kitty">DM me</div>
     // </div>
+
+    <div className="demo">
+      <header className="demo__header">Cat Tinder</header>
+      <div className="demo__content">
+        <div className="demo__card-cont">
+          <div className="demo__card">
+            <div className="demo__card__top">
+              <img src={url} className="demo__card__img"></img>
+            </div>
+            <div className="demo__card__btm">
+              <p className="demo__card__we">DM me</p>
+            </div>
+            <div className="demo__card__choice m--reject"></div>
+            <div className="demo__card__choice m--like"></div>
+            <div className="demo__card__drag"></div>
+          </div>
+        </div>
+        <p className="demo__tip">Swipe left or right</p>
+      </div>
+    </div>
   );
 };
 
--- cat-tinder-noredux/src/animation-logic.js
+++ cat-tinder-noredux/src/animation-logic.js
@@ -1,6 +1,7 @@
 import $ from jquery;
 
-$(document).ready(function () {
+$(document).ready(function() {
+
   var animating = false;
   var cardsCounter = 0;
   var numOfCards = 6;
@@ -12,77 +13,68 @@ $(document).ready(function () {
   function pullChange() {
     animating = true;
     deg = pullDeltaX / 10;
-    $card.css(
-      'transform',
-      'translateX(' + pullDeltaX + 'px) rotate(' + deg + 'deg)'
-    );
+    $card.css("transform", "translateX("+ pullDeltaX +"px) rotate("+ deg +"deg)");
 
     var opacity = pullDeltaX / 100;
-    var rejectOpacity = opacity >= 0 ? 0 : Math.abs(opacity);
-    var likeOpacity = opacity <= 0 ? 0 : opacity;
-    $cardReject.css('opacity', rejectOpacity);
-    $cardLike.css('opacity', likeOpacity);
-  }
+    var rejectOpacity = (opacity >= 0) ? 0 : Math.abs(opacity);
+    var likeOpacity = (opacity <= 0) ? 0 : opacity;
+    $cardReject.css("opacity", rejectOpacity);
+    $cardLike.css("opacity", likeOpacity);
+  };
 
   function release() {
+
     if (pullDeltaX >= decisionVal) {
-      $card.addClass('to-right');
+      $card.addClass("to-right");
     } else if (pullDeltaX <= -decisionVal) {
-      $card.addClass('to-left');
+      $card.addClass("to-left");
     }
 
     if (Math.abs(pullDeltaX) >= decisionVal) {
-      $card.addClass('inactive');
+      $card.addClass("inactive");
 
-      setTimeout(function () {
-        $card.addClass('below').removeClass('inactive to-left to-right');
+      setTimeout(function() {
+        $card.addClass("below").removeClass("inactive to-left to-right");
         cardsCounter++;
         if (cardsCounter === numOfCards) {
           cardsCounter = 0;
-          $('.demo__card').removeClass('below');
+          $(".demo__card").removeClass("below");
         }
       }, 300);
     }
 
     if (Math.abs(pullDeltaX) < decisionVal) {
-      $card.addClass('reset');
+      $card.addClass("reset");
     }
 
-    setTimeout(function () {
-      $card
-        .attr('style', '')
-        .removeClass('reset')
-        .find('.demo__card__choice')
-        .attr('style', '');
+    setTimeout(function() {
+      $card.attr("style", "").removeClass("reset")
+        .find(".demo__card__choice").attr("style", "");
 
       pullDeltaX = 0;
       animating = false;
     }, 300);
-  }
+  };
 
-  $(document).on(
-    'mousedown touchstart',
-    '.demo__card:not(.inactive)',
-    function (e) {
-      if (animating) return;
+  $(document).on("mousedown touchstart", ".demo__card:not(.inactive)", function(e) {
+    if (animating) return;
 
-      $card = $(this);
-      $cardReject = $('.demo__card__choice.m--reject', $card);
-      $cardLike = $('.demo__card__choice.m--like', $card);
-      var startX = e.pageX || e.originalEvent.touches[0].pageX;
+    $card = $(this);
+    $cardReject = $(".demo__card__choice.m--reject", $card);
+    $cardLike = $(".demo__card__choice.m--like", $card);
+    var startX =  e.pageX || e.originalEvent.touches[0].pageX;
 
-      $(document).on('mousemove touchmove', function (e) {
-        var x = e.pageX || e.originalEvent.touches[0].pageX;
-        pullDeltaX = x - startX;
-        if (!pullDeltaX) return;
-        pullChange();
-      });
+    $(document).on("mousemove touchmove", function(e) {
+      var x = e.pageX || e.originalEvent.touches[0].pageX;
+      pullDeltaX = (x - startX);
+      if (!pullDeltaX) return;
+      pullChange();
+    });
 
-      $(document).on('mouseup touchend', function () {
-        $(document).off('mousemove touchmove mouseup touchend');
-        if (!pullDeltaX) return; // prevents from rapid click events
-        release();
-      });
-    }
-  );
+    $(document).on("mouseup touchend", function() {
+      $(document).off("mousemove touchmove mouseup touchend");
+      if (!pullDeltaX) return; // prevents from rapid click events
+      release();
+    });
+  });
 });
