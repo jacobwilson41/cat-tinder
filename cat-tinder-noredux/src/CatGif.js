ca739471787c96c145c59dc7fa590ecfbb518ae4
Jacob Wilson <jacobcharles96@gmail.com>
2020-06-30 11:07:59 -0400

    AFTER DEADLINE: centered gif

--- cat-tinder-noredux/src/CatGif.js
+++ cat-tinder-noredux/src/CatGif.js
@@ -7,20 +7,24 @@ const CatGif = ({ url, getGif }) => {
   };
 
   return (
-    <div className="demo">
-      <header className="demo__header"></header>
-      <div className="demo__content">
-        <div className="demo__card-cont">
-          <div className="demo__card">
-            <img onClick={handleClick} src={url} alt="cat"></img>;
-            <div className="demo__card__choice m--reject"></div>
-            <div className="demo__card__choice m--like"></div>
-            <div className="demo__card__drag"></div>
-          </div>
-        </div>
-        <p className="demo__tip">Swipe left or right</p>
-      </div>
+    <div className="flex-container">
+      <img onClick={handleClick} src={url} alt="cat"></img>
     </div>
+
+    // <div className="demo">
+    //   <header className="demo__header"></header>
+    //   <div className="demo__content">
+    //     <div className="demo__card-cont">
+    //       <div className="demo__card">
+    //         <img onClick={handleClick} src={url} alt="cat"></img>;
+    //         <div className="demo__card__choice m--reject"></div>
+    //         <div className="demo__card__choice m--like"></div>
+    //         <div className="demo__card__drag"></div>
+    //       </div>
+    //     </div>
+    //     <p className="demo__tip">Swipe left or right</p>
+    //   </div>
+    // </div>
   );
 };
 
